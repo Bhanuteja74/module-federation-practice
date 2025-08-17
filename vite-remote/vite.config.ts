985826fa753361c97d304cc288488e6c8401fb78
Bhanu Teja Kommi <145745009+Bhanuteja74@users.noreply.github.com>
2025-08-17 17:36:15 +0530

    Add Button component and exposing that component

--- vite-remote/vite.config.ts
+++ vite-remote/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    federation({
+      name: 'vite-remote',
+      filename: 'index.js',
+      exposes: {
+        './Button': './src/Button.tsx',
+      },
+      shared: ['react', 'react-dom'],
+      })
+  ],
 })
